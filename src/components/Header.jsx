7581a7b4bb510ed8c051f54ab0473d5f9d02d25d
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-04-24 21:45:57 +0200

    MUI init, folder structure init

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+const Header = () => {
+  return <Button style={{ backgroundColor: "orange" }}>dff</Button>;
+};
+
+export default Header;
