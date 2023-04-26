07ce46933fdd34d10a348320d20a0759b4a7c0d4
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-04-26 23:27:05 +0200

    added support dropdown with icons and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full left-0 ">
-      <hr className="border-1 border-red-400 " />
+      <hr className="  styledHr" />
       <div className="h-9 bg-gray-900 text-center text-sm ">
         <span className="text-white mt-1">&#169; 2023 </span>
         <span className="text-red-500">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import gothic_icon from "../assets/gothic_icon.png";
+import paypal from "../assets/paypal.png";
+import patronite from "../assets/patronite.png";
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+  const [supportOpen, setSupportOpen] = useState(false);
 
   const handleDropDown = () => {
     setOpen(!isOpen);
+  };
+
+  const handleSupport = () => {
+    setSupportOpen(!supportOpen);
   };
 
   const handleDefinitiveEd = () => {
@@ -14,7 +21,7 @@ const Header = () => {
 
   return (
     <div>
-      <nav class="flex items-center justify-between flex-wrap bg-amber-500 hover:bg-red-600 hover:font-bold transition-colors duration-500 p-6 ">
+      <nav class="flex items-center justify-between flex-wrap bg-amber-500 hover:bg-red-600 hover:font-bold transition-colors duration-500 p-4 ">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <img src={gothic_icon} style={{ height: 42, width: 42 }} />
           <span class="font-bold text-3xl tracking-tight text-amber-950">
@@ -24,23 +31,92 @@ const Header = () => {
 
         <div id="nav-menu" class="w-full block flex-grow lg:flex lg:w-auto  ">
           <div class="text-base lg:flex-grow mt-4">
+            <button
+              class=" text-center inline-flex flex-wrap items-center  lg:mt-0 text-slate-900 hover:text-white mr-4 "
+              onClick={handleSupport}
+            >
+              <svg
+                class="w-6 h-6 mr-0.5"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                ></path>
+              </svg>
+              Wesprzyj
+              <svg
+                class="w-4 h-4 ml-0.5"
+                aria-hidden="true"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </button>
+
+            <div
+              class={`z-10 fixed ${
+                supportOpen ? "block" : "hidden"
+              } bg-white rounded-lg shadow w-40 dark:bg-gray-700  `}
+            >
+              <ul class="h-24 py-2 overflow-y-auto text-gray-700 dark:text-gray-200">
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    <img class="w-6 h-6 mr-2 " src={paypal} alt="Jese image" />
+                    PayPal
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    <img
+                      class="w-6 h-6 mr-2 rounded-full"
+                      src={patronite}
+                      alt="Jese image"
+                    />
+                    Patronite
+                  </a>
+                </li>
+              </ul>
+            </div>
+
             <a
               href="#responsive-header"
-              class=" block mt-4 lg:inline-block lg:mt-0 text-slate-900 hover:text-white mr-4"
+              class="text-center inline-flex flex-wrap items-centerlg:mt-0 text-slate-900 hover:text-white mr-4"
             >
-              Dokumentacja
-            </a>
-            <a
-              href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-slate-900 hover:text-white mr-4"
-            >
+              <svg
+                class="w-6 h-6 mr-0.5"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2.0"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                ></path>
+              </svg>
               Księga gości
-            </a>
-            <a
-              href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-slate-900 hover:text-white mr-4"
-            >
-              Patronite
             </a>
           </div>
           <div className="mr-10">
@@ -103,7 +179,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <hr className="border-2 border-gray-700" />
+      <hr className="styledHr" style={{ height: 3 }} />
     </div>
   );
 };
