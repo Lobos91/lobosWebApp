5000b9333dac29c4a6fe3044fe2560c571c078d4
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-07-24 14:07:38 +0200

    t

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,7 @@
 import i18n from "i18next";
-import XHR from "i18next-xhr-backend";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-
-const xhr = new XHR();
-xhr.init(null, options);
 
 i18n
   .use(Backend)
