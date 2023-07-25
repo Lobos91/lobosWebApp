ab28d60670cfe1bc6a7be65ad80456a8d9dbf492
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-07-25 08:45:23 +0200

    translation fix

--- src/i18n.js
+++ src/i18n.js
@@ -10,7 +10,7 @@ i18n
   .init({
     lng: "pl",
     backend: {
-      loadPath: "../src/translations/{{lng}}.json",
+      loadPath: "/src/translations/{{lng}}.json",
     },
     fallbackLng: "pl",
     debug: false,
