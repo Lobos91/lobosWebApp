03a86958f0553846079aeec6cc7808e18b572ec5
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-07-28 08:25:22 +0200

    i18next

--- src/i18n.js
+++ src/i18n.js
@@ -3,17 +3,28 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
+import common_pl from "./translations/pl/common.json";
+import common_en from "./translations/en/common.json";
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     lng: "pl",
-    backend: {
-      // loadPath: "src/translations/{{lng}}.json",
-
-      loadPath: "/gothic2lobos.com/locales/{{lng}}.json",
+    resources: {
+      pl: {
+        common: common_pl,
+      },
+      en: {
+        common: common_en,
+      },
     },
+    // backend: {
+    //   // loadPath: "src/translations/{{lng}}.json",
+
+    //   loadPath: "/gothic2lobos.com/locales/{{lng}}.json",
+    // },
     fallbackLng: "pl",
     debug: true,
     interpolation: {
